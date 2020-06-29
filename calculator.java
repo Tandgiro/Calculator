af83cea2689e62b61369f0d1d4b934a4d88689bc
Tandgiro <67535434+Tandgiro@users.noreply.github.com>
2020-06-29 17:25:09 +0300

    calculator V2.5

--- calculator.java
+++ calculator.java
@@ -21,29 +21,43 @@ class func{
 
 public class calculator {
     public static void main(String [] args){
-        double z = 1, x = 2, v = 3, n = 4;
-        Scanner one_tap = new Scanner(System.in);
-        int a;
-        System.out.println("Enter First value: ");
-        a = one_tap.nextInt();
+        boolean close = true;
+        for (;close = true; ) {
 
-        Scanner two_tap = new Scanner(System.in);
-        int b ;
-        System.out.println("Enter First value: ");
-        b = one_tap.nextInt();
+            double z = 1, x = 2, v = 3, n = 4;
+            Scanner one_tap = new Scanner(System.in);
+            int a;
+            System.out.println("Enter First value: ");
+            a = one_tap.nextInt();
 
-        Scanner three_tap = new Scanner(System.in);
-        int c;
-        System.out.println("1(+), 2(-), 3(*), 4(/) : ");
-        c = one_tap.nextInt();
+            Scanner two_tap = new Scanner(System.in);
+            int b;
+            System.out.println("Enter First value: ");
+            b = one_tap.nextInt();
 
-        if (c == z)
-            System.out.println("Equally: " + (a + b));
-        else if (c == x)
-            System.out.println("Equally: " + (a - b));
-        else if (c == v)
-            System.out.println("Equally: " + (a * b));
-        else if (c == n)
-            System.out.println("Equally: " + (a / b));
+            Scanner three_tap = new Scanner(System.in);
+            int c;
+            System.out.println("1(+), 2(-), 3(*), 4(/) : ");
+            c = one_tap.nextInt();
+
+            if (c == z)
+                System.out.println("Equally: " + (a + b));
+            else if (c == x)
+                System.out.println("Equally: " + (a - b));
+            else if (c == v)
+                System.out.println("Equally: " + (a * b));
+            else if (c == n)
+                System.out.println("Equally: " + (a / b));
+            Scanner cl = new Scanner(System.in);
+            int stop;
+            System.out.println("Close application 'calculator'? |(1)/Yes | (0)/No| : ");
+            stop = cl.nextInt();
+
+            if (stop == 1)
+                close = false;
+
+
+        }
+
     }
 }
